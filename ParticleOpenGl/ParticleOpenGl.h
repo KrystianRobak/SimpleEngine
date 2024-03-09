49c21cbbef13f2cff7e4a14753614a862135f7ea
Krystian Robak <122100595+KrystianRobak@users.noreply.github.com>
2024-03-09 23:09:41 +0100

    Dodaj pliki projektów.

---
+++ ParticleOpenGl/ParticleOpenGl.h
@@ -0,0 +1,74 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "ShaderLoader.h"
+
+int main(int argc, char** argv) {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "Triangle", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
+
+    // Define the vertex data for the triangle
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+         0.0f,  1.0f, 0.0f,
+    };
+
+    // Create and bind a Vertex Array Object
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders("vertexShader.vertexshader", "fragmentShader.fragmentshader");
+
+
+    // Main loop    
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        // Draw the triangle
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
